a8c846fe03ac1cab9275943141461af54a59876e
Roman Lebetskiy <romanlebetskiy@gmail.com>
2022-09-16 05:57:55 +0300

    basic0p task

--- codewars.js
+++ codewars.js
@@ -1,3 +1,12 @@
 function isDivisible(n, x, y) {
   return (n%x == 0 && n%y ==0) ? true : false;
 
+function basicOp(operation, value1, value2)
+
+  {
+  if(operation == '+')return value1 + value2;
+  if(operation == '-')return value1 - value2;
+  if(operation == '*')return value1 * value2;
+  if(operation == '/')return value1 / value2;
+}
+
